5c5284ef549be636fb563cb414fb65f47d6bcbae
waramavictor <victorkinyua16@gmail.com>
2024-07-15 02:08:07 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,49 +1,4 @@
 
-// import React from 'react';
-// import './App.css';
-// import { Routes, Route, Navigate } from 'react-router-dom';
-// import BookList from "./components/BookList";
-// import BookDetail from "./components/BookDetail";
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
-// import Favorites from './components/Favorites';
-// import Login from './components/Login';
-// import { useAppContext } from './components/context/appContext';
-
-// function App() {
-//   const { isAuthenticated } = useAppContext();
-
-//   return (
-//     <div className="App">
-//       {isAuthenticated ? (
-//         <>
-//           <Navbar />
-//           <Routes>
-//             <Route path="/" element={<BookList />} />
-//             <Route path="/books/:id" element={<BookDetail />} />
-//             <Route path="/favorites" element={<Favorites />} />
-//             <Route path="*" element={<Navigate to="/" />} />
-//           </Routes>
-//           <Footer />
-//         </>
-//       ) : (
-//         <Routes>
-//           <Route path="/login" element={<Login />} />
-//           <Route path="*" element={<Navigate to="/login" />} />
-//         </Routes>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
 // App.js
 import React from 'react';
 import './App.css';
